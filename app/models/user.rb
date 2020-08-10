80d2788a918d986515ec49d31c88cfa8fb54e354
Danilo de Lima <danilolicd@gmail.com>
2020-08-10 17:30:39 -0300

    add validates to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@
 
 class User < ApplicationRecord
   has_secure_password
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'with validations' do
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
