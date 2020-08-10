e2edda47e02435ef862faa85bec081c562148b8f
Danilo de Lima <danilolicd@gmail.com>
2020-08-10 18:00:39 -0300

    add endpoint to create a session

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Api
+  class SessionsController < ApplicationController
+    def create
+      @user = User.find_by!(email: params[:email])
+      if @user.authenticate(params[:password])
+        @session = JWTSessions::Session.new(payload: create_payload(@user))
+        render json: @session.login
+      else
+        render json: 'Invalid user', status: :unauthorized
+      end
+    end
+
+    private
+
+    def create_payload(user)
+      { user_id: user.id, role: user.role }
+    end
+  end
+end
