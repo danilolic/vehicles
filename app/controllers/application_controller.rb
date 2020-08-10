8b1a21eceac06cfdee7c7325a904c0d495627873
Danilo de Lima <danilolicd@gmail.com>
2020-08-10 18:02:23 -0300

    add current_user helper method and add rescue when user is not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  private
+
+  def not_authorized
+    render json: { error: 'Not authorized' }, status: :unauthorized
+  end
+
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
 end
