73b097e050cc1400208e1153aebfc7fc0f28b55e
Danilo de Lima <danilolicd@gmail.com>
2020-08-11 18:16:52 -0300

    create seeds with user admin and brands

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'admin', email: 'admin@email.com', password: '123456')
+
+20.times do
+  Brand.create(name: "#{Faker::Vehicle.make} - #{Faker::Vehicle.vin}")
+end
