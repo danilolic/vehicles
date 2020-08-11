afea2c96ab765050cb42d4c3e121d1ea84c31ac3
Danilo de Lima <danilolicd@gmail.com>
2020-08-11 17:51:15 -0300

    add brand scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :brands
   namespace :api do
     resource :session, only: %i[create]
     resource :refresh, only: %i[create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_203512) do
+ActiveRecord::Schema.define(version: 2020_08_11_203017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brands", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_brands_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
