66554afe9e07e95ef2459ad7ddd783cd40a1bdc0
Danilo de Lima <danilolicd@gmail.com>
2020-08-12 15:27:14 -0300

    refactor model spec

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Model, type: :model do
     create(:model)
   end
 
-  describe 'with validations' do
+  describe 'with relationships' do
     it { is_expected.to belong_to(:brand) }
+  end
+
+  describe 'with validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
   end
