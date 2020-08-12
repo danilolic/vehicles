da64e4c75c0760322a936358394350853d32fc0a
Danilo de Lima <danilolicd@gmail.com>
2020-08-12 15:26:05 -0300

    brand has_many models

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Brand < ApplicationRecord
+  has_many :models
   validates :name, presence: true, uniqueness: true
 end
--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
+  describe 'with relationships' do
+    it { is_expected.to have_many(:models) }
+  end
+
   describe 'with validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
